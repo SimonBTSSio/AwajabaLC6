03dad20699c27b104c6a5579bf897e79022ed89a
SimonBTSSio <Simon.farnault.sio@gmail.com>
2020-09-04 16:47:53 +0200

    version 1 Repas

---
+++ Repas.java
@@ -0,0 +1,10 @@
+/*
+import java.time.LocalDate;
+class Repas {
+	private int numero;
+	private LocalDate date;
+	Repas(int numero, LocalDate date){
+		this.numero = numero;
+		this.date = date;
+	}
+}*/
